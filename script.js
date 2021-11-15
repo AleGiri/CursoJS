946cc979c5e33d1e148d9bd38dd875d79ee1b20f
Alejandro Gonzalez Giri <alejandrogonzalezgiri@gmail.com>
2021-11-15 00:43:31 -0300

    Merge

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 alert("Bienvenido a SIMULADOR DE BATALLA")
-let difficulty = prompt("Elija la dificultad: ¿facil, mediana o dificil?")
+let difficulty = prompt("Elija la dificultad: ¿facil, mediana o dificil?").toLowerCase();
 while (difficulty != "facil" && difficulty != "mediana" && difficulty != "dificil") {
     alert("¡Elija una dificultad!")
-    difficulty = prompt("Elija la dificultad: ¿facil, mediana o dificil?")
+    difficulty = prompt("Elija la dificultad: ¿facil, mediana o dificil?").toLowerCase();
 }
 
 
@@ -11,26 +11,27 @@ if (difficulty == "facil") {
     plyrDmgBlock = 4;
     enemyDmg = 4;
     enemyDmgBlock = 2;
-    remHpPlyr = 20
-    remHpEnemy = 25
+    remHpPlyr = 20;
+    remHpEnemy = 25;
     enemy = "duende"
 } else if (difficulty == "mediana") {
     plyrDmg = 5;
     plyrDmgBlock = 4;
     enemyDmg = 5;
     enemyDmgBlock = 2;
-    remHpPlyr = 25
-    remHpEnemy = 35
+    remHpPlyr = 25;
+    remHpEnemy = 35;
     enemy = "ogro"
 } else if (difficulty == "dificil") {
     plyrDmg = 15;
     plyrDmgBlock = 11;
     enemyDmg = 16;
     enemyDmgBlock = 6;
-    remHpPlyr = 60
-    remHpEnemy = 100
-    enemy = "demonio"
+    remHpPlyr = 60;
+    remHpEnemy = 100;
+    enemy = "demonio";
 }
+
 alert("Ha seleccionado " + difficulty + ". ¡Su batalla sera contra un " + enemy + "!")
 let battle;
 
@@ -54,64 +55,22 @@ function enemyHpBlock() {
     remHpEnemy = remHpEnemy - plyrDmgBlock
 }
 
-
-
-if (difficulty = "facil") {
-    while (remHpPlyr > 0 && remHpEnemy > 0) {
-        battle = prompt("El " + enemy + " tiene " + remHpEnemy + " de vida y hace " + enemyDmg + " de daño con cada ataque. Tu tienes " + remHpPlyr + " de vida y haces " + plyrDmg + " con cada ataque. Elige A para Atacar o B para bloquear ¿Quieres atacar o bloquear?");
-        switch (battle) {
-            case battle = "a":
-                plyrHpAttack();
-                enemyHpAttack();
-                battleState();
-                break;
-            case battle = "b":
-                plyrHpBlock();
-                enemyHpBlock();
-                battleState();
-                break;
-            default:
-                alert("¡Debes elegir A para atacar o B para bloquear!");
-                break;
-        }
-    }
-} else if (difficulty == "mediana") {
-    while (remHpPlyr > 0 && remHpEnemy > 0) {
-        battle = prompt("El " + enemy + " tiene " + remHpEnemy + " de vida y hace " + enemyDmg + " de daño con cada ataque. Tu tienes " + remHpPlyr + " de vida y haces " + plyrDmg + " con cada ataque. Elige A para Atacar o B para bloquear ¿Quieres atacar o bloquear?");
-        switch (battle) {
-            case battle = "a":
-                plyrHpAttack();
-                enemyHpAttack();
-                battleState();
-                break;
-            case battle = "b":
-                plyrHpBlock();
-                enemyHpBlock();
-                battleState();
-                break;
-            default:
-                alert("¡Debes elegir A para atacar o B para bloquear!");
-                break;
-        }
-    }
-} else if (difficulty == "dificil") {
-    while (remHpPlyr > 0 && remHpEnemy > 0) {
-        battle = prompt("El duende tiene " + remHpEnemy + " de vida y hace " + enemyDmg + " de daño con cada ataque. Tu tienes " + remHpPlyr + " de vida y haces " + plyrDmg + " con cada ataque. Elige A para Atacar o B para bloquear ¿Quieres atacar o bloquear?");
-        switch (battle) {
-            case battle = "a":
-                plyrHpAttack();
-                enemyHpAttack();
-                battleState();
-                break;
-            case battle = "b":
-                plyrHpBlock();
-                enemyHpBlock();
-                battleState();
-                break;
-            default:
-                alert("¡Debes elegir A para atacar o B para bloquear!");
-                break;
-        }
+while (remHpPlyr > 0 && remHpEnemy > 0) {
+    battle = prompt("El " + enemy + " tiene " + remHpEnemy + " de vida y hace " + enemyDmg + " de daño con cada ataque. Tu tienes " + remHpPlyr + " de vida y haces " + plyrDmg + " con cada ataque. Elige A para Atacar o B para bloquear ¿Quieres atacar o bloquear?").toLowerCase();
+    switch (battle) {
+        case battle = "a":
+            plyrHpAttack();
+            enemyHpAttack();
+            battleState();
+            break;
+        case battle = "b":
+            plyrHpBlock();
+            enemyHpBlock();
+            battleState();
+            break;
+        default:
+            alert("¡Debes elegir A para atacar o B para bloquear!");
+            break;
     }
 }
 
