1d5439f136c20f5a1809816d5be694ef671c2313
Alejandro Gonzalez Giri <alejandrogonzalezgiri@gmail.com>
2021-11-16 23:29:38 -0300

    Agregado Arrays desafio .sort

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 alert("Bienvenido a SIMULADOR DE BATALLA")
 let difficulty = prompt("Elija la dificultad: ¿facil, mediana o dificil?").toLowerCase();
-while (difficulty != "facil" && difficulty != "mediana" && difficulty != "dificil") {
+while (difficulty !== "facil" && difficulty !== "mediana" && difficulty !== "dificil") {
     alert("¡Elija una dificultad!")
     difficulty = prompt("Elija la dificultad: ¿facil, mediana o dificil?").toLowerCase();
 }
-class Enemy{
-    constructor(plyrDmg, plyrDmgBlock, enemyDmg, enemyDmgBlock, remHpPlyr, remHpEnemy, nombre){
-    this.plyrDmg = plyrDmg;
-    this.plyrDmgBlock = plyrDmgBlock;
-    this.enemyDmg = enemyDmg;
-    this.enemyDmgBlock = enemyDmgBlock;
-    this.remHpPlyr = remHpPlyr;
-    this.remHpEnemy = remHpEnemy;
-    this.nombre = nombre
+class Enemy {
+    constructor(plyrDmg, plyrDmgBlock, enemyDmg, enemyDmgBlock, remHpPlyr, remHpEnemy, nombre) {
+        this.plyrDmg = plyrDmg;
+        this.plyrDmgBlock = plyrDmgBlock;
+        this.enemyDmg = enemyDmg;
+        this.enemyDmgBlock = enemyDmgBlock;
+        this.remHpPlyr = remHpPlyr;
+        this.remHpEnemy = remHpEnemy;
+        this.nombre = nombre
+    }
 }
-}
-const duende= new Enemy (6,4,4,2,20,25,"duende");
-const ogro= new Enemy (5,4,5,2,25,35,"ogro");
-const demonio= new Enemy (15,11,11,6,60,100,"demonio");
+
+const duende = new Enemy(25, 4, 4, 2, 20, 25, "duende");
+const ogro = new Enemy(5, 4, 5, 2, 25, 35, "ogro");
+const demonio = new Enemy(15, 11, 11, 6, 60, 100, "demonio");
 
 let enemies = [
     duende,
@@ -26,18 +27,21 @@ let enemies = [
 
 enemies.push(demonio)
 
-
-let dif=[]
+let dif = []
 
 if (difficulty == "facil") {
-    dif=enemies[0]
+    dif = enemies[0]
 } else if (difficulty == "mediana") {
-    dif=enemies[1]
+    dif = enemies[1]
 } else if (difficulty == "dificil") {
-    dif=enemies[2]
+    dif = enemies[2]
 }
+
 console.log(enemies)
 console.log(dif)
+
+let sortArray = enemies.sort((a, c) => a.nombre.length - c.nombre.length);
+console.log("Array", sortArray)
 
 alert("Ha seleccionado " + difficulty + ". ¡Su batalla sera contra un " + dif.nombre + "!")
 let battle;
@@ -65,12 +69,12 @@ function enemyHpBlock() {
 while (dif.remHpPlyr > 0 && dif.remHpEnemy > 0) {
     battle = prompt("El " + dif.nombre + " tiene " + dif.remHpEnemy + " de vida y hace " + dif.enemyDmg + " de daño con cada ataque. Tu tienes " + dif.remHpPlyr + " de vida y haces " + dif.plyrDmg + " con cada ataque. Elige A para Atacar o B para bloquear ¿Quieres atacar o bloquear?").toLowerCase();
     switch (battle) {
-        case battle = "a":
+        case "a":
             plyrHpAttack();
             enemyHpAttack();
             battleState();
             break;
-        case battle = "b":
+        case "b":
             plyrHpBlock();
             enemyHpBlock();
             battleState();
