e80f733b4e1c55bc7e198e486cc5291cb174151b
Alejandro Gonzalez Giri <alejandrogonzalezgiri@gmail.com>
2021-12-21 04:49:46 -0300

    Agregado de botones funcionales, login, cambio de sprites y cambios de estetica

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+
+if (localStorage.getItem("Registrado") !== "true") {
+    let playerNameInput = prompt("Ingrese su nombre");
+    localStorage.setItem("Nombre", playerNameInput);
+    document.getElementById("playerName").value = localStorage.getItem("Nombre");
+    localStorage.setItem("Registrado", "true")
+} else {
+    document.getElementById("playerName").value = localStorage.getItem("Nombre")
+}
+
+credentialsButton.onclick = () => {
+    playerNameInput = prompt("¿Que nombre desea usar?")
+    localStorage.setItem("Nombre", playerNameInput);
+    document.getElementById("playerName").value = localStorage.getItem("Nombre")
+}
+
 // Constructor de enemigos
 class Enemy {
     constructor(plyrDmg, plyrDmgBlock, enemyDmg, enemyDmgBlock, name) {
@@ -9,9 +25,9 @@ class Enemy {
     }
 }
 
-const duende = new Enemy(10, 4, 4, 2, "duende");
-const ogro = new Enemy(5, 4, 5, 2, "ogro");
-const demonio = new Enemy(15, 10, 12, 6, "demonio");
+const duende = new Enemy(10, 4, 4, 2, "Duende");
+const ogro = new Enemy(5, 4, 5, 2, "Ogro");
+const demonio = new Enemy(15, 10, 12, 6, "Demonio");
 
 let enemies = [
     duende,
@@ -27,6 +43,42 @@ let difficulty = [
     remHpPlyr,
     remHpEnemy,
 ]
+let darkMode = false
+darkModeButton.onclick = () => {
+    let bgMode = document.getElementById("battleFrame")
+    let boardMode = document.getElementById("actionBar")
+    switch (darkMode) {
+        case true:
+            bgMode.style = 'background-image: url(images/battlebg.jpg);'
+            boardMode.style = "background-image: url(images/backboard.png)"
+            darkMode = false
+            console.log("Dark Mode:" + darkMode)
+            break;
+        case false:
+            bgMode.style = 'background-image: url(images/battlebgnight.jpg);'
+            boardMode.style = "background-image: url(images/backboardnight.png)"
+            darkMode = true
+            console.log("Dark Mode:" + darkMode)
+    }
+}
+let modelSex = "ModelMasc"
+modelButton.onclick = () => {
+    let modelChange = document.getElementById("war")
+    switch (modelSex) {
+        case "ModelFem":
+            modelChange.src = "images/warriormgif.gif"
+            modelSex = "ModelMasc"
+            console.log(modelSex)
+            break;
+        case "ModelMasc":
+            modelChange.src = "images/warriorfgif.gif"
+            modelSex = "ModelFem"
+            console.log(modelSex)
+    }
+}
+
+
+
 
 easy.onclick = () => {
     difficulty = enemies[0]
@@ -36,8 +88,9 @@ easy.onclick = () => {
     let enemySprite = document.getElementById("enemyType")
     enemySprite.removeChild(enemySprite.childNodes[2])
     enemySprite = document.createElement("img")
-    enemySprite.src = "images/goblin.png";
+    enemySprite.src = "images/goblingif.gif";
     document.getElementById("enemyType").appendChild(enemySprite)
+    document.getElementById("enemyName").value = difficulty.name
 }
 medium.onclick = () => {
     difficulty = enemies[1]
@@ -47,8 +100,9 @@ medium.onclick = () => {
     let enemySprite = document.getElementById("enemyType")
     enemySprite.removeChild(enemySprite.childNodes[2])
     enemySprite = document.createElement("img")
-    enemySprite.src = "images/ogre.png";
+    enemySprite.src = "images/ogregif.gif";
     document.getElementById("enemyType").appendChild(enemySprite)
+    document.getElementById("enemyName").value = difficulty.name
 }
 hard.onclick = () => {
     difficulty = enemies[2]
@@ -58,8 +112,9 @@ hard.onclick = () => {
     let enemySprite = document.getElementById("enemyType")
     enemySprite.removeChild(enemySprite.childNodes[2])
     enemySprite = document.createElement("img")
-    enemySprite.src = "images/demon.png";
+    enemySprite.src = "images/demongif.gif";
     document.getElementById("enemyType").appendChild(enemySprite)
+    document.getElementById("enemyName").value = difficulty.name
 }
 // Fin seleccion de dificultad
 
@@ -68,23 +123,27 @@ hard.onclick = () => {
 
 
 function battleState() {
-    document.getElementById("printRecap").value = ("El " + difficulty.name + " tiene " + difficulty.remHpEnemy + " de vida y tu tienes " + difficulty.remHpPlyr + " de vida.")
+    // Funcion que agrega cantidad de vida restante del jugador y del enemigo y su nombre a un cuadro donde se actualiza despues de cada accion
+    document.getElementById("printRecap").value = ("El " + difficulty.name.toLowerCase() + " tiene " + difficulty.remHpEnemy + " de vida y tu tienes " + difficulty.remHpPlyr + " de vida.")
 }
 
 function attackAction() {
+    // Funcion para determinar la vida del enemigo y jugador despues de un ataque
     difficulty.remHpPlyr = difficulty.remHpPlyr - difficulty.enemyDmg;
     difficulty.remHpEnemy = difficulty.remHpEnemy - difficulty.plyrDmg
 }
 
 function blockAction() {
+    // Funcion para determinar la vida del enemigo y jugador despues de un bloqueo
     difficulty.remHpPlyr = difficulty.remHpPlyr - difficulty.enemyDmgBlock;
     difficulty.remHpEnemy = difficulty.remHpEnemy - difficulty.plyrDmgBlock
 }
 
 function difficultySelect() {
+    // Funcion que agrega los valores de vida, valores de daño y tipo de enemigo y del jugador como mensaje inicial
     document.getElementById("playerHP").value = difficulty.remHpPlyr;
     document.getElementById("enemyHP").value = difficulty.remHpEnemy;
-    document.getElementById("printRecap").value = ("¡Su batalla sera contra un " + difficulty.name + "!")
+    document.getElementById("printRecap").value = ("¡Su batalla sera contra un " + difficulty.name.toLowerCase() + "!")
     document.getElementById("printAttributes").value = ("Atacar: haces " + difficulty.plyrDmg + " de daño y recibes " + difficulty.enemyDmg + " daño\nBloquear: haces " + difficulty.plyrDmgBlock + " de daño y recibes " + difficulty.enemyDmgBlock + " de daño")
 }
 
@@ -101,19 +160,21 @@ let loseCount = localStorage.getItem("Derrotas")
 function winPrint() {
     document.getElementById("wins").value = winCount
 }
+
 function losePrint() {
     document.getElementById("loses").value = loseCount
 }
 
 
 function battleResult() {
+    // Funcion para determinar si el jugador o el enemigo murio, agregando un mensaje de victoria o derrota y genera un localStorage que guarda la cantidad total de victorias y derrotas en el panel inferior
     if (difficulty.remHpEnemy <= 0 && difficulty.remHpPlyr > 0) {
         document.getElementById("printRecap").value = ("¡Has ganado! Selecciona nueva dificultad para pelear nuevamente.");
 
         if (localStorage.getItem("Victorias") == null && localStorage.getItem("Victorias") == 0) {
             localStorage.setItem("Victorias", 0)
-            } else {
-            winCount = JSON.parse(localStorage.getItem('Victorias'))+1
+        } else {
+            winCount = JSON.parse(localStorage.getItem('Victorias')) + 1
             localStorage.setItem('Victorias', JSON.stringify(winCount))
         }
         winPrint()
@@ -123,7 +184,7 @@ function battleResult() {
         if (localStorage.getItem("Derrotas") == null && localStorage.getItem("Derrotas") == 0) {
             localStorage.setItem("Derrotas", 0)
         } else {
-            loseCount = JSON.parse(localStorage.getItem('Derrotas'))+1
+            loseCount = JSON.parse(localStorage.getItem('Derrotas')) + 1
             localStorage.setItem('Derrotas', JSON.stringify(loseCount))
         }
         losePrint()
